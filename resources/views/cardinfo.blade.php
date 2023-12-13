477a41c9ea35a32b4df4bfcd5d8f39e148a8be64
Fznaf <89885429+Fznaf@users.noreply.github.com>
2023-12-14 01:41:55 +0700

    Update cardinfo.blade.php

--- resources/views/cardinfo.blade.php
+++ resources/views/cardinfo.blade.php
@@ -10,20 +10,33 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @foreach ($cards as $card)
-                    <div class="text-gray-900 dark:text-gray-100 space-y-6">
-                        <img src="{{ $card['images']['small'] }}" alt = "{{__("Card Picture")}}">
+                    <div class="flex flex-row text-gray-900 dark:text-gray-100 space-y-6">
+                        <img style="margin-right: 16px; margin-bottom: 16px" src="{{ $card['images']['small'] }}" alt = "{{__("Card Picture")}}">
+                        <div class="text-gray-900 dark:text-gray-100 space-y-6">
                         <h2>{{$card['name']}}</h2>
-                        {{$card['id']}}
+                        <p>Card ID: {{$card['id']}}</p>
+                            <p>Super Type: {{$card['supertype']}}</p>
+                            <p> Types: @foreach ($card['types'] as $type)
+                                {{ $type }}
+                        @endforeach</p>
+                                <p> Subtype: @foreach ($card['subtypes'] as $subtype)
+                        {{ $subtype }}
+                        @endforeach</p>
+                        <p> HP: {{$card['hp']}}</p>
+                        </div>  
 
-                        <hr>
+                        
                     @endforeach
-                    <form method="POST" action="{{ route('favorite') }}">
-                        @csrf
-                        <input type="checkbox" name="favoriteCard" value="{{$card['id']}}" onchange="this.form.submit()">
-                        <input type="hidden" name="image" value="{{$card['images']['small']}}">
-                        <input type="hidden" name="cardname" value="{{$card['name']}}">
-                     </form>
+                    
                 </div>
+                <form method="POST" action="{{ route('favorite') }}">
+                    @csrf
+                    <input type="checkbox" name="favoriteCard" value="{{$card['id']}}" onchange="this.form.submit()">
+                    Mark as Favorite
+                    <input type="hidden" name="image" value="{{$card['images']['small']}}">
+                    <input type="hidden" name="cardname" value="{{$card['name']}}">
+                 </form>
+                 <hr>
             </div>
         </div>
     </div>
